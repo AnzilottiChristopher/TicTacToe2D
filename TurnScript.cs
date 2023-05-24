a5eb5eaf1c1033ec78bb3893843ede56685f377b
Chris Anzilotti <anzilottichr@gmail.com>
2023-05-24 01:36:50 -0400

    Finished the tutorial. Still needs win condition

--- TurnScript.cs
+++ TurnScript.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TurnScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private SpriteRenderer spriteRenderer;
+   public Sprite[] images;
+   private GameObject gameBoard;
+   private bool unplayed = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Start()
+   {
+      spriteRenderer.sprite = null;
+   }
+
+   private void OnMouseDown()
+   {
+      if (unplayed)
+      {
+         int index = gameBoard.GetComponent<GameScript>().playerTurn();
+         spriteRenderer.sprite = images[index];
+         unplayed = false;
+      }
+   }
+
+   private void Awake()
+   {
+      spriteRenderer = GetComponent<SpriteRenderer>();
+      gameBoard = GameObject.Find("GameBoard");
+      
+   }
 }
